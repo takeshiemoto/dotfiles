f3228867f79ba579a3ed587654eb40419fc3c376
takeshiemoto <takeshi.emoto@chatwork.com>
2024-10-05 22:34:18 +0900

    Add Docker to install.sh script
    Updated the script to include Docker as part of the applications to be installed. This ensures that all necessary development tools are included in the installation process.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 brew update
 
 # アプリケーションをインストール
-brew install --cask raycast ghq peco iterm2 jetbrains-toolbox google-chrome 1password brave-browser
+brew install --cask raycast ghq peco iterm2 jetbrains-toolbox google-chrome 1password brave-browser docker
 brew install tmux neovim
 
 echo "Installation is complete!"
